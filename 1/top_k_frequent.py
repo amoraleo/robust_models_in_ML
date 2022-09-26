0405df42f6c09bea08083e21b3d8f0355c6bf920
Morozov Leonid <MorLeoAlex2000@yandex.ru>
2022-09-27 00:48:01 +0300

    top_k_frequent.py added

---
+++ 1/top_k_frequent.py
@@ -0,0 +1,28 @@
+nums  = [int(i) for i in input("Enter the list values separated by a space: ").split()]
+k = int(input("Enter k value: "))
+
+num_dict = {}
+
+for num in nums:
+    num_dict[num] = 1 + num_dict.get(num, 0)
+
+freq_dict = {}
+
+for num, freq in num_dict.items():
+    if freq not in freq_dict:
+        freq_dict[freq] = [num]
+    else:
+        freq_dict[freq].append(num)
+
+result = []
+
+for i in range(len(nums), 0, -1):
+    if len(result) >= k:
+        break
+    if i in freq_dict:
+        #if len(freq_dict[i]) + len(result) > k:
+            #result += freq_dict[i][:k-len(result)]
+            #break
+        result += freq_dict[i]
+
+print(result)
